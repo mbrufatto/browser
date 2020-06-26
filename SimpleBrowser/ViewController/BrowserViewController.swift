4e6e3160b8ba0c9478fb271e7e4b491fa9bdc3c2
Márcio Habigzang Brufatto <mhbrufatto@gmail.com>
2020-06-26 02:03:31 -0300

    - Abrindo na página inicial definida para o site da Apple.

--- SimpleBrowser/ViewController/BrowserViewController.swift
+++ SimpleBrowser/ViewController/BrowserViewController.swift
@@ -7,24 +7,50 @@
 //
 
 import UIKit
+import WebKit
 
 class BrowserViewController: UIViewController {
-
+    
+    @IBOutlet weak var urlAddress: UISearchBar!
+    @IBOutlet weak var toolBar: UIToolbar!
+    
+    var webView: WKWebView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        initialSetup()
+        addWebView()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    private func initialSetup() {
+        webView = WKWebView()
+        webView.navigationDelegate = self
+        self.view.addSubview(webView)
+        
+        let urlString = "https://www.apple.com.br"
+        self.urlAddress.text = urlString
+        let url = URL(string: urlString)!
+        webView.load(URLRequest(url: url))
+        webView.allowsBackForwardNavigationGestures = true
     }
-    */
 
+    private func addWebView() {
+        self.webView.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            self.webView.topAnchor.constraint(equalTo: self.urlAddress.bottomAnchor),
+            self.webView.leadingAnchor.constraint(equalTo: self.view.leadingAnchor),
+            self.webView.trailingAnchor.constraint(equalTo: self.view.trailingAnchor),
+            self.webView.bottomAnchor.constraint(equalTo: self.toolBar.topAnchor)
+        ])
+    }
+}
+
+extension BrowserViewController: WKNavigationDelegate {
+    
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        title = webView.title
+        self.urlAddress.text = webView.url?.absoluteString
+    }
 }
