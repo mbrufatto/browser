0c04fdce9b1ca2cd40f97ac883ee607fcdb51794
Márcio Habigzang Brufatto <mhbrufatto@gmail.com>
2020-06-26 00:51:45 -0300

    Gitignore adicionado

---
+++ SimpleBrowser/ViewController/ListSiteViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ListSiteViewController.swift
+//  SimpleBrowser
+//
+//  Created by Marcio Habigzang Brufatto on 26/06/20.
+//  Copyright © 2020 Mantra Tech. All rights reserved.
+//
+
+import UIKit
+
+class ListSiteViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
